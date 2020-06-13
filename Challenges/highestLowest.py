d78dcefddc83fbf6bb1b760ff4ba4f116824f6a3
Brian <brinaldi000@gmail.com>
2020-06-13 12:34:47 -0400

    testing git

--- Challenges/highestLowest.py
+++
@@ -1,5 +0,0 @@
-def high_and_low(numbers):
-    '''finds the highest and lowest numbers in a string of space seperated numbers'''
-    return numbers
-
-print(high_and_low(("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), "542 -214"))
